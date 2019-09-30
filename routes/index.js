f3a103ca696442ad0f46d085de7c82a43be290cf
안재철 <qnzo8888@gmail.com>
2019-10-01 07:05:27 +0900

    alarm

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var sequelize = require('sequelize');
+const { User } = require('../models');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+  var companys = await User.findAll({
+    where: {flag: true},
+    order: [['dona_count', 'DESC']],
+    limit: 5
+  })
+
+  var persons = await User.findAll({
+    where: {flag: false},
+    order: [['dona_count', 'DESC']],
+    limit: 5
+  })
+
+  var result = {companys: companys, persons: persons};
+
   if(req.user == undefined)
-    res.render('main', {
+    res.render('main', Object.assign(result,{
       register:null, 
       logged:false,
-      login:null
-    });
+      login:null,
+    }))
   else
-    res.render('main', Object.assign(req.user, {
+    res.render('main', Object.assign(result, req.user, {
       register:null, 
       logged:true,
-      login:null
+      login:null,
     })); 
 });
-
-// router.get('/', function(req, res, next) {
-//   res.render('main', Object.assign(req.user, {register:null}));
-// });
 
 router.get('/test', function(req, res, next) {
   res.render('test');
